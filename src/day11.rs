942178dc38db8693e89092f620c10e09f5335254
banditpig <mike_k_houghton@yahoo.co.uk>
2023-12-19 10:16:07 +0000

    Day 11 start

--- src/day11.rs
+++ src/day11.rs
@@ -1,11 +1,28 @@
+use num_traits::abs;
 use std::arch::aarch64::veor_s8;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt::{Display, Formatter};
+use strum_macros::Display;
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+impl Point {
+    pub fn point(pair: (usize, usize)) -> Self {
+        Point {
+            x: pair.0,
+            y: pair.1,
+        }
+    }
+}
 struct Image {
     image: Vec<Vec<char>>,
-    max_col: usize,
-    max_row: usize,
+    maxx: usize,
+    maxy: usize,
+    map: HashMap<Point, char>,
+    galaxies: Vec<Point>,
 }
 impl Display for Image {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -22,7 +39,25 @@ impl Image {
     pub fn expand(&mut self) {
         self.expand_rows();
         self.expand_columns();
+        self.maxy = self.image.len();
+        self.maxx = self.image.first().unwrap().len();
     }
+    pub fn create_map(&mut self) {
+        let mut map = HashMap::new();
+        let mut galaxies = vec![];
+        for (iy, row) in self.image.iter().enumerate() {
+            for (ix, ch) in row.iter().enumerate() {
+                let p = Point::point((ix, iy));
+                map.insert(p, *ch);
+                if (*ch == '#') {
+                    galaxies.push(p);
+                }
+            }
+        }
+        self.galaxies = galaxies;
+        self.map = map;
+    }
+
     fn expand_rows(&mut self) {
         let mut ixs = vec![];
         for (ix, r) in self.image.iter().enumerate() {
@@ -32,14 +67,14 @@ impl Image {
         }
         let mut prior = 0;
         for (prior, ix) in ixs.iter().enumerate() {
-            self.image.insert(ix + prior, vec!['.'; self.max_col]);
+            self.image.insert(ix + prior, vec!['.'; self.maxx]);
         }
     }
 
     fn expand_columns(&mut self) {
         //take vertical slice
         let mut col_ixs = vec![];
-        for col in (0..self.max_col) {
+        for col in (0..self.maxx) {
             let mut temp = vec![];
             for row in &self.image {
                 temp.push(row.get(col).unwrap())
@@ -54,6 +89,29 @@ impl Image {
                 row.insert(col_ix + prior, '.');
             }
         }
+    }
+
+    pub fn distance(&self, p1: Point, p2: Point) -> usize {
+        let (x1, y1) = (p1.x, p1.y);
+        let (x2, y2) = (p2.x, p2.y);
+
+        x1.abs_diff(x2) + y1.abs_diff(y2)
+    }
+    pub fn pair_galaxies(&self) -> Vec<(Point, Point)> {
+        let mut pairs = vec![];
+        let mut ix = 1;
+        let mut current_g = self.galaxies.get(0).unwrap();
+        for pointer in (ix..self.galaxies.len()) {
+            for ixx in (pointer..self.galaxies.len()) {
+                let p = (*current_g, *self.galaxies.get(ixx).unwrap());
+                pairs.push(p);
+            }
+            current_g = self.galaxies.get(ix).unwrap();
+            ix += 1;
+        }
+
+        pairs
+        //todo!()
     }
 }
 
@@ -72,23 +130,33 @@ fn load_input() -> Image {
 
     Image {
         image,
-        max_col,
-        max_row,
+        maxx: max_col,
+        maxy: max_row,
+        galaxies: vec![],
+        map: HashMap::new(),
     }
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     use itertools::Itertools;
-
+    use std::sync::{Arc, Mutex};
+    use std::thread;
     #[test]
-    fn load() {
+    fn galaxy_pairs() {
         let mut g = load_input();
 
-        println!("{}", g);
         println!();
         g.expand();
+        g.create_map();
+        let pairs = g.pair_galaxies();
+        let mut sum = 0;
 
-        println!("{}", g);
+        for (s, d) in pairs {
+            let d = sum += g.distance(s, d);
+        }
+
+        println!("{:?}", sum);
     }
+    //(1,6) (5,11)
 }
